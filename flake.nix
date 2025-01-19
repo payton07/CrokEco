e16621f23f7a985af67f54d8a311c878464da546
Ewen Philippot <ewen.philippot@etu.umontpellier.fr>
2025-01-19 14:38:34 +0100

    Add: dev front + specs un peu

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  languages = {
+                    java = {
+                      enable = true;
+                      gradle.enable = true;
+                      jdk.package = pkgs.jdk17;
+                    };
+                    javascript = {
+                      enable = true;
+                      npm.enable = true;
+                    };
+                    typescript.enable = true;
+                  };
+
+                  env.ANDROID_HOME="/home/$USER/Android/Sdk";
+
+                  scripts = {
+                    build.exec = ''
+                      cd packages/frontend
+                      npx expo prebuild
+                      cd android
+                      ./gradlew app:assembleRelease
+                    '';
+                  };
+
+                  git-hooks.hooks = {
+                    actionlint.enable = true;
+                    denolint = {
+                      enable = true;
+                      excludes = [ ".js" ];
+                    };
+                    nixpkgs-fmt.enable = true;
+                    nil.enable = true;
+                    prettier.enable = true;
+                  };
+                }
+              ];
+            };
+          });
+    };
+}
