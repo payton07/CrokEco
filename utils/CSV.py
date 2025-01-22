ec73e9bebe3f524b408cd55dc2d40a13867a9225
MaIlory <84338982+MaIlory@users.noreply.github.com>
2025-01-22 19:15:52 +0100

    Fonction filtrage base carbonne

--- utils/CSV.py
+++ utils/CSV.py
@@ -1,25 +1,75 @@
 import pandas as pd
+from math import isnan
 
-data=pd.read_csv("C:/Users/barat/Document/Projet_L3/projet-Co2Score/assets/base-carboner.csv", sep=',')
+INPUT_FILE = "assets/base-carboner.csv"
+OUTPUT_FILE = "assets/base_carboner_filtre.csv"
+USELESS_KEYS = ["Type Ligne", "Structure", "Type de l'élément", "Statut de l'élément", "Contributeur","Programme","Url du programme","Localisation géographique"]
+# Il faudra peut etre en lever le NaN et mettre toutes les catégories à supprimer ici car avec d'autres valeurs on ne sait pas ce que la fonction rique de renvoyer
 
-T1=[]
-T2=[]
-T3=[]
+def categorie(data_in):
+    T=[]
+    i=0
+    for elt in data_in["Code de la catégorie"]:
+            if "Achats de biens > Produits agro-alimentaires, plats préparés et boissons" in elt:
+                T.append(i)
+            elif "Achats de biens > Produits de l'agriculture et de la pêche" in elt:
+                T.append(i)
+            i+=1
 
-i=0
-for elt in data["Code de la catégorie"]:
-    if "Achats de biens > Produits agro-alimentaires, plats préparés et boissons" in elt:
-        T1.append(i)
-    elif "Achats de biens > Produits de l'agriculture et de la pêche" in elt:
-        T2.append(i)
-    elif "Achats de services > Restauration" in elt:
-        T3.append(i)
-    i+=1
+    resultat = data_in.loc[T]
+    
+    # Actualisation des indexs de chaque ligne du resultat
+    resultat.index = pd.RangeIndex(start=0, stop=len(resultat["Code de la catégorie"]), step=1)
 
-resultat1 = data.loc[T1]
-resultat2 = data.loc[T2]
-resultat3 = data.loc[T3]
+    return resultat
 
-resultat1.to_csv('base_carboner_produits_agro.csv', index=False)
-resultat2.to_csv('base_carboner_agricultureetpeche.csv', index=False)
-resultat3.to_csv('base_carboner_restauration.csv', index=False)
+
+def archive(data_in):
+    T=[]
+    i=0
+    for elt in data_in["Statut de l'élément"]:
+        if not("Archivé" in elt):
+            T.append(i)
+        i+=1
+    
+    resultat = data_in.loc[T]
+
+    # Actualisation des indexs de chaque ligne du resultat
+    resultat.index = pd.RangeIndex(start=0, stop=len(resultat["Statut de l'élément"]), step=1)
+
+    return resultat
+
+
+def suppr_nan(data_in):
+    data_out = data_in.copy()
+
+    for key in data_in.keys():
+        delete=True
+        for elt in data_in[key]:
+            if type(elt)!=str:
+                if not(isnan(elt)):
+                    delete=False
+            else:
+                delete=False
+        if delete:
+            del data_in[key]
+    return data_in
+
+def suppr_keys(data_in, keys):
+    data_out = data_in.copy()
+
+    for key in keys:
+        try:
+            del data_in[key]
+        except KeyError:
+            print("Mauvaise clé")
+    
+    return data_out
+
+if __name__ == '__main__':
+    data=pd.read_csv(INPUT_FILE, sep=',')
+    data=categorie(data)
+    data=archive(data)
+    data=suppr_nan(data)
+    suppr_keys(data, USELESS_KEYS)
+    data.to_csv(OUTPUT_FILE, index=False)
