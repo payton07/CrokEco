b52b4c66dc7d1b45a2db95679f8ff837467f64f2
MaIlory <84338982+MaIlory@users.noreply.github.com>
2025-01-30 14:04:39 +0100

    tous les ingredients s'ajoute a la bd

---
+++ specs/CodeE_A3.sql
@@ -0,0 +1,99 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE "Plat" (
+  "Ciqual_AGB" VARCHAR(10),
+  "Nom" VARCHAR(30),
+  "ID_categorie" VARCHAR(20),
+  "Nom_Francais" VARCHAR(50),
+  "Groupe_d_aliment" VARCHAR(50),
+  "Sous_groupe_d_aliment" VARCHAR(50),
+  "LCI_Name" VARCHAR(50),
+  CONSTRAINT PK_PLAT PRIMARY KEY("Ciqual_AGB"),
+  CONSTRAINT FK_PLAT_CATEGORIE FOREIGN KEY ("ID_categorie") REFERENCES Categorie("ID_categorie")
+);
+
+CREATE TABLE "Ingredients" (
+  "ID_ingredient" INTEGER PRIMARY KEY,
+  "Ciqual_AGB" VARCHAR(10),
+  /*"Nom_Francais" VARCHAR(50),
+  "Groupe_d_aliment" VARCHAR(50),
+  "Sous_groupe_d_aliment" VARCHAR(50),
+  "LCI_Name" VARCHAR(50),*/
+  "Ingredient" VARCHAR(50),
+  "Score_unique_EF" DECIMAL(10,8),
+  "Changement_climatique" DECIMAL(10,8),
+  "Appauvrissement_de_la_couche_d_ozone" DECIMAL(10,8),
+  "Rayonnements_ionisants" DECIMAL(10,8),
+  "Formation_photochimique_d_ozone" DECIMAL(10,8),
+  "Particules_fines" DECIMAL(10,8),
+  "Effets_toxicologiques_sur_la_sante_humaine__substances_non_cancerogenes" DECIMAL(10,8),
+  "Effets_toxicologiques_sur_la_sante_humaine__substances_cancerogenes" DECIMAL(10,8),
+  "Acidification_terrestre_et_eaux_douces" DECIMAL(10,8),
+  "Eutrophisation_eaux_douces" DECIMAL(10,8),
+  "Eutrophisation_marine" DECIMAL(10,8),
+  "Eutrophisation_terrestre" DECIMAL(10,8),
+  "Ecotoxicite_pour_ecosystemes_aquatiques_d_eau_douce" DECIMAL(10,8),
+  "Utilisation_du_sol" DECIMAL(10,8),
+  "Epuisement_des_ressources_eau" DECIMAL(10,8),
+  "Epuisement_des_ressources_energetiques" DECIMAL(10,8),
+  "Epuisement_des_ressources_mineraux" DECIMAL(10,8),
+  /*CONSTRAINT PK_ELEMENT PRIMARY KEY("ID_element"),*/
+  CONSTRAINT FK_PLAT_CATEGORIE FOREIGN KEY ("Ciqual_AGB") REFERENCES Plat("Ciqual_AGB")
+);
+
+CREATE TABLE "Categorie" (
+  "ID_categorie" INT(6),
+  "NomC" VARCHAR(20),
+  CONSTRAINT PK_CATEGORIE PRIMARY KEY("ID_categorie"),
+  CONSTRAINT FK_CATEGORIE_PLAT FOREIGN KEY ("ID_categorie") REFERENCES "Plat" ("ID_categorie")
+);
+
+CREATE TABLE "Tags" (
+  "ID_tags" INT(6),
+  "Type" VARCHAR(3),
+  "Valeur" VARCHAR(30),
+  CONSTRAINT PK_TAGS PRIMARY KEY("ID_tags")
+);
+
+CREATE TABLE "Recherche" (
+  "ID_recherche" INT(6),
+  "Text_request" VARCHAR(30),
+  "Resultat" VARCHAR(30),
+  "Temps" time,
+  "Date" Date,
+  "ID_plat" INT(6),
+  "ID_histo" INT(6),
+  CONSTRAINT PK_RECHERCHE PRIMARY KEY("ID_recherche"),
+  CONSTRAINT FK_RECHERCHE_PLAT FOREIGN KEY ("ID_plat") REFERENCES "Plat" ("ID_plat"),
+  CONSTRAINT FK_RECHERCHE_HISTORIQUE FOREIGN KEY ("ID_histo") REFERENCES "Historique" ("ID_historique")
+);
+
+CREATE TABLE "Historique" (
+  "ID_historique" INT(6),
+  "Date_creation" Date,
+  CONSTRAINT PK_HISTORIQUE PRIMARY KEY("ID_historique")
+);
+
+CREATE TABLE "Menu" (
+  "ID_menu" INT(6),
+  "NomM" VARCHAR(20),
+  "Etablissement" VARCHAR(20),
+  "Localisation" VARCHAR(30),
+  CONSTRAINT PK_MENU PRIMARY KEY("ID_menu")
+);
+
+CREATE TABLE "Menu_Compose_Plat" (
+  "ID_menu" INT(6),
+  "ID_plat" INT(6),
+  CONSTRAINT PK_MENU_COMPOSE_PLAT PRIMARY KEY ("ID_menu", "ID_plat"),
+  CONSTRAINT FK_Menu_Compose_Plat__MENU FOREIGN KEY ("ID_menu") REFERENCES "Menu" ("ID_menu"),
+  CONSTRAINT FK_Menu_Compose_Plat__PLAT FOREIGN KEY ("ID_plat") REFERENCES "Plat" ("ID_plat")
+);
+
+CREATE TABLE "Designe_Tags"(
+  "ID_tags" INT(6),
+  "ID_plat" INT(6),
+  CONSTRAINT PK_DESIGNE_TAGS PRIMARY KEY ("ID_tags", "ID_plat"),
+  CONSTRAINT FK_Designe_Tags__TAGS FOREIGN KEY ("ID_tags") REFERENCES "Tags" ("ID_tags"),
+  CONSTRAINT FK_Designe_Tags__PLAT FOREIGN KEY ("ID_plat") REFERENCES "Plat" ("ID_plat")
+);
---
+++ utils/ingredient_CSV.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from math import isnan
+import sqlite3
+import os
+
+INPUT_FILE = "assets/Agribalyse_Detail_ingredient.csv"
+OUTPUT_FILE = "assets/Agribalyse_Detail_ingredient_v2.csv"
+INPUT_SQL = "specs/CodeE_A3.sql"
+OUTPUT_DB = "assets/ingredient_carbon_score.db"
+USELESS_KEYS = ["Ciqual  code"]
+
+def suppr_keys(data_in, keys):
+    data_out = data_in.copy()
+
+    for key in keys:
+        try:
+            del data_in[key]
+        except KeyError:
+            print("Mauvaise clé")
+    
+    return data_out
+
+def export_sqlite(input_sql,output_db):
+    if os.path.isfile(output_db):
+        os.remove(output_db)
+
+    connection = sqlite3.connect(output_db)
+    cursor = connection.cursor()
+    with open(input_sql) as script_sql_file:
+        script_sql = script_sql_file.read()
+        cursor.executescript(script_sql)
+    connection.commit()
+    connection.close()
+
+
+def insertion_bd(dico_in, output_db):
+    suppr_keys(dico_in, ["Nom Français", "LCI Name", "Sous-groupe d'aliment", "Groupe d'aliment"])
+    data = list(dico_in.itertuples(index=False, name=None))
+    con = sqlite3.connect(output_db)
+    cur = con.cursor()
+    cur.executemany("""INSERT INTO Ingredients(Ciqual_AGB,Ingredient,Score_unique_EF,Changement_climatique,
+                    Appauvrissement_de_la_couche_d_ozone,Rayonnements_ionisants,Formation_photochimique_d_ozone,Particules_fines,
+                    Effets_toxicologiques_sur_la_sante_humaine__substances_non_cancerogenes,Effets_toxicologiques_sur_la_sante_humaine__substances_cancerogenes,
+                    Acidification_terrestre_et_eaux_douces,Eutrophisation_eaux_douces,Eutrophisation_marine,Eutrophisation_terrestre,
+                    Ecotoxicite_pour_ecosystemes_aquatiques_d_eau_douce,Utilisation_du_sol,Epuisement_des_ressources_eau,Epuisement_des_ressources_energetiques,
+                    Epuisement_des_ressources_mineraux) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", data)
+    con.commit()
+    con.close()
+
+
+if __name__ == '__main__':
+    data=pd.read_csv(INPUT_FILE, sep=',')
+    suppr_keys(data, USELESS_KEYS)
+    export_sqlite(INPUT_SQL,OUTPUT_DB)
+    insertion_bd(data.copy(),OUTPUT_DB)
+
+    con = sqlite3.connect(OUTPUT_DB)
+    cur = con.cursor()
+    cur.execute("SELECT ID_ingredient,Ciqual_AGB,Ingredient FROM Ingredients;")
+    res = cur.fetchall()
+    print(res)
+    con.close()
+
+    #data.to_csv(OUTPUT_FILE, index=False)
