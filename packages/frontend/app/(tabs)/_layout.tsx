972e917fd1fab5b66ff9721a4dcafad712713e7b
Philippot Ewen <ewen.philippot@etu.umontpellier.fr>
2025-01-30 15:24:16 +0100

    fix(frontend): supprime test_bdd et le met dans assets/bdd pour le conserver

--- packages/frontend/app/(tabs)/_layout.tsx
+++ packages/frontend/app/(tabs)/_layout.tsx
@@ -55,12 +55,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="bdd_test"
-        options={{
-          title: "Bdd test",
-        }}
-      />
     </Tabs>
   );
 }
