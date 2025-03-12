208ada4b57e6db9335277031872044ef6f660dbd
= <patrice.keglo@etu.umontpellier.fr>
2025-03-12 02:01:03 +0100

    UPDATE : Fonctionne avec android

--- packages/frontend/app/(tabs)/index.tsx
+++ packages/frontend/app/(tabs)/index.tsx
@@ -1,15 +1,33 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet , Alert, Image as RNImage} from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Tesseract from 'tesseract.js';
+import TextRecognition from "@react-native-ml-kit/text-recognition";
 
 export default function Scanner(){
+const [text ,setText] = useState('Rien');
+const imagePath = require('../../assets/ingImages/image4.png'); 
+const imageUri = RNImage.resolveAssetSource(imagePath).uri;
+
+async function recognizeText(){
+  try {
+    const result = await TextRecognition.recognize(imageUri);
+    setText(result.text);
+    console.log("Text :",text,"result :", result.text);
+    
+  } catch (error) {
+    Alert.alert('Erreur', 'Échec de la reconnaissance de texte');
+    console.log("Erreur OCR :", error);
+  }
+};
+
   return (
     <SafeAreaProvider>
     <View style={styles.container}>
-      <Text style={styles.title}>Scanner page</Text>
+      <Text style={styles.title} onPress={recognizeText}> Scanner page </Text>
       <View
         style={styles.separator}
         lightColor="#eee"
