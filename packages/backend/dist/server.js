7d1ccce57a7ba5ad92f46fac4e4ab3e81a2ce96a
= <patrice.keglo@etu.umontpellier.fr>
2025-03-21 14:27:25 +0100

    Avant le merge sur dev

--- packages/backend/dist/server.js
+++ packages/backend/dist/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+// import { get } from 'http';
+// import { getPlats } from './bdd';
 const app = express();
 const port = 3000;
-// Middleware pour parser le corps des requêtes en JSON
+// Middleware pour parser le JSON
 app.use(express.json());
 // Route de base
 app.get('/', (req, res) => {
@@ -12,9 +14,24 @@ let users = [
     { id: 1, name: 'Alice', email: 'alice@example.com' },
     { id: 2, name: 'Bob', email: 'bob@example.com' },
 ];
-// Récupérer tous les utilisateurs
-app.get('/api/users', (req, res) => {
-    res.json(users);
+// Récupérer tous les plats
+// app.get('/api/plats', (req: Request, res: Response) => {
+//   const data = getPlats();
+//   // res.send("bien vue");
+//   res.json(data);
+// });
+//Récupérer un plat par son ID
+app.get('/api/plats/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const donne = { id_plat: id };
+    // const data = getPlats(donne,false,false,false);
+    // if (data !== undefined &&data.length > 0) {
+    //   const plat = data[0];
+    //   res.status(201).json(plat);
+    // } else {
+    //   res.status(404).json({ message: 'Plat not found' });
+    // }
+    res.send("bien bien");
 });
 // Ajouter un utilisateur
 app.post('/api/users', (req, res) => {
