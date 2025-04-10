44a7dd110991ba6a0df9f2e977a00ff216a3d0a1
= <patrice.keglo@etu.umontpellier.fr>
2025-04-11 01:15:17 +0200

    Toujours du Css , logo + coup de prettier

--- packages/backend/utils/other.ts
+++ packages/backend/utils/other.ts
@@ -1,4 +1,4 @@
-export const HOST = '192.168.1.129';
+export const HOST = "192.168.1.129";
 // export const HOST = '172.24.10.186';
 export const PORT = 3000;
 // const IP = "127.0.0.1";
--- packages/backend/utils/script.ts
+++ packages/backend/utils/script.ts
@@ -1,17 +1,17 @@
-import fs from 'fs';
-import crypto from 'crypto';
-import path from 'path';
+import fs from "fs";
+import crypto from "crypto";
+import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function addKeyIfNotExist(envPath : string, secretKey: string){
+function addKeyIfNotExist(envPath: string, secretKey: string) {
   try {
     if (fs.existsSync(envPath)) {
-      const Contenu = fs.readFileSync(envPath, 'utf-8');
-      
-      if (!Contenu.includes('SECRET_KEY=')) {
+      const Contenu = fs.readFileSync(envPath, "utf-8");
+
+      if (!Contenu.includes("SECRET_KEY=")) {
         fs.appendFileSync(envPath, `\nSECRET_KEY=${secretKey}\n`);
         console.log(`Clé secrète générée et ajoutée à ${envPath}`);
       } else {
@@ -21,18 +21,21 @@ function addKeyIfNotExist(envPath : string, secretKey: string){
       console.log(`Le fichier .env n'existe pas à l'emplacement ${envPath}`);
     }
   } catch (err) {
-    console.error(`Erreur lors de l'ajout de la clé secrète à ${envPath}:`, err);
+    console.error(
+      `Erreur lors de l'ajout de la clé secrète à ${envPath}:`,
+      err
+    );
   }
 }
 
 function genereSecretKey() {
-  const secretKey = crypto.randomBytes(32).toString('hex');
+  const secretKey = crypto.randomBytes(32).toString("hex");
 
-  const envBackend = path.resolve(__dirname, '../.env');
-  const envFrontend = path.resolve(__dirname, '../../frontend/.env');
+  const envBackend = path.resolve(__dirname, "../.env");
+  const envFrontend = path.resolve(__dirname, "../../frontend/.env");
 
-  addKeyIfNotExist(envBackend,secretKey);
-  addKeyIfNotExist(envFrontend,secretKey);
+  addKeyIfNotExist(envBackend, secretKey);
+  addKeyIfNotExist(envFrontend, secretKey);
 }
 
 genereSecretKey();
