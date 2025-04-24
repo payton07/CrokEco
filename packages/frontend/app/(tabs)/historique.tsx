18821cdfc92fb6e4cb3a050a5bdf873085249da0
= <patrice.keglo@etu.umontpellier.fr>
2025-04-24 03:35:23 +0200

    Remplacement du logo de l'appli + prettier

--- packages/frontend/app/(tabs)/historique.tsx
+++ packages/frontend/app/(tabs)/historique.tsx
@@ -66,7 +66,7 @@ export default function History() {
     }, []),
   );
 
-  function sortPlats(){
+  function sortPlats() {
     const res = menus.sort((a, b) => {
       const parseDate = (str: string) => {
         const [day, month, year] = str.split("/");
